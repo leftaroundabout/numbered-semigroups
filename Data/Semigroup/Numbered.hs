4d4b0c72eeedc14dffd0e964a0592f9de320b39a
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-10-15 21:39:08 +0200

    Inital commit.

---
+++ Data/Semigroup/Numbered.hs
@@ -0,0 +1,12 @@
+-- |
+-- Module      : Data.Semigroup.Numbered
+-- Copyright   : (c) Justus Sagemüller 2017
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsagemue $ uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+module Data.Semigroup.Numbered where
+
